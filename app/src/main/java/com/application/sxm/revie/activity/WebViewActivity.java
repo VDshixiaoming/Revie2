36a95da2294be90a935864886f4c937c6fc04a50
sxm <SXM@shixiaomingdeMacBook-Pro.local>
2018-11-01 12:05:01 +0800

    webview配置

--- app/src/main/java/com/application/sxm/revie/activity/WebViewActivity.java
+++ app/src/main/java/com/application/sxm/revie/activity/WebViewActivity.java
@@ -1,11 +1,18 @@
 package com.application.sxm.revie.activity;
 
 
+import android.util.Log;
+import android.view.KeyEvent;
+
 import com.alibaba.android.arouter.facade.annotation.Autowired;
 import com.alibaba.android.arouter.facade.annotation.Route;
 import com.alibaba.android.arouter.launcher.ARouter;
 import com.application.sxm.revie.R;
+import com.application.sxm.revie.model.AppConstants;
+import com.tencent.smtt.sdk.WebChromeClient;
+import com.tencent.smtt.sdk.WebSettings;
 import com.tencent.smtt.sdk.WebView;
+import com.tencent.smtt.sdk.WebViewClient;
 
 import butterknife.BindView;
 
@@ -30,7 +37,54 @@ public class WebViewActivity extends BaseActivity{
     @Override
     public void initView() {
         ARouter.getInstance().inject(this);
+        initWebView();
+    }
 
+    private void initWebView() {
+        WebSettings settings = mWebView.getSettings();
+        settings.setJavaScriptEnabled(true);
         mWebView.loadUrl(mLoadUrl);
+        mWebView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView webView, String url) {
+                webView.loadUrl(url);
+                return true;
+            }
+
+            @Override
+            public void onReceivedError(WebView webView, int i, String s, String s1) {
+                Log.i(AppConstants.LOG_WEB_MSG, "网页加载失败！");
+            }
+        });
+        //进度条
+        mWebView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public void onProgressChanged(WebView webView, int i) {
+
+            }
+        });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mWebView != null) {
+            mWebView.destroy();
+        }
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        /**
+         * 拦截返回键
+         */
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            if (mWebView != null && mWebView.canGoBack()) {
+                mWebView.goBack();
+                return true;
+            }
+            return super.onKeyDown(keyCode, event);
+        }
+        return super.onKeyDown(keyCode, event);
     }
 }
