eb38f788469dc7419861e6d12d99a385bf20ed54
sxm <245943105@qq.com>
2018-11-02 10:30:09 +0800

    接入Loger和JsBridge

--- app/src/main/java/com/application/sxm/revie/application/BaseAppliaction.java
+++ app/src/main/java/com/application/sxm/revie/application/BaseAppliaction.java
@@ -1,11 +1,17 @@
 package com.application.sxm.revie.application;
 
 import android.app.Application;
-import android.util.Log;
+import android.support.annotation.Nullable;
 
 import com.alibaba.android.arouter.launcher.ARouter;
+import com.application.sxm.revie.BuildConfig;
+import com.application.sxm.revie.R;
 import com.application.sxm.revie.model.AppConstants;
 import com.facebook.drawee.backends.pipeline.Fresco;
+import com.orhanobut.logger.AndroidLogAdapter;
+import com.orhanobut.logger.FormatStrategy;
+import com.orhanobut.logger.Logger;
+import com.orhanobut.logger.PrettyFormatStrategy;
 import com.tencent.smtt.sdk.QbSdk;
 
 /**
@@ -40,7 +46,23 @@ public class BaseAppliaction extends Application {
     private void initBaseSDK() {
         Fresco.initialize(this);
         ARouter.init(this);
+        initLogger();
         initTbs();
+    }
+
+    /**
+     * 初始化Logger
+     */
+    private void initLogger() {
+        FormatStrategy formatStrategy = PrettyFormatStrategy.newBuilder()
+                .tag(getResources().getString(R.string.app_name))
+                .build();
+        Logger.addLogAdapter(new AndroidLogAdapter(formatStrategy) {
+            @Override
+            public boolean isLoggable(int priority, @Nullable String tag) {
+                return BuildConfig.DEBUG;
+            }
+        });
     }
 
     /**
@@ -50,12 +72,12 @@ public class BaseAppliaction extends Application {
         QbSdk.PreInitCallback cb = new QbSdk.PreInitCallback() {
             @Override
             public void onCoreInitFinished() {
-                Log.i(AppConstants.LOG_WEB_MSG, "X5初始化完成");
+                Logger.t(AppConstants.LOG_WEB_MSG).i("X5初始化完成");
             }
 
             @Override
             public void onViewInitFinished(boolean b) {
-                Log.i(AppConstants.LOG_WEB_MSG, String.format("X5初始化%s", b ? "成功" : "失败"));
+                Logger.t(AppConstants.LOG_WEB_MSG).i(String.format("X5初始化%s", b ? "成功" : "失败"));
             }
         };
         QbSdk.initX5Environment(getApplicationContext(), cb);
